62ab466b43f8f454afe040aa76ac279c4b687392
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-04-02 11:46:17 +0530

    download page skeleton made

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,12 +1,14 @@
-import { NavBar } from '@/components/ui/tubelight-navbar'
-import React from 'react'
+import DownloadSection from "@/components/DownloadSection";
+import { NavBar } from "@/components/ui/tubelight-navbar";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            <NavBar />
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavBar />
+      <DownloadSection />
+    </div>
+  );
+};
 
-export default page
+export default page;
