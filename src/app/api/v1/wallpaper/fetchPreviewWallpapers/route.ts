05ca304d039fe76a35ada8d8979e00081cb59d4a
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-05-10 10:56:01 +0530

    cached fetchWallpapers into localStorage, only removing cahce on create and delete wallpaper

--- src/app/api/v1/wallpaper/fetchPreviewWallpapers/route.ts
+++ src/app/api/v1/wallpaper/fetchPreviewWallpapers/route.ts
@@ -1,59 +1,69 @@
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const { userId } = await req.json()
+  const { userId } = await req.json();
 
-    try {
-        const wallpapers = await prisma.userWallpaper.findMany({
-            where: { userId },
-            select: {
-                id: true,
-                platform: true,
-                githubUsername: true,
-                bentoLink: true,
-                backgroundImageLink: true,
-            }
-        })
+  try {
+    const wallpapers = await prisma.userWallpaper.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        platform: true,
+        githubUsername: true,
+        bentoLink: true,
+        backgroundImageLink: true,
+      },
+    });
 
-        type FormattedWallpaper = {
-            wallpaperId: string;
-            githubUsername: string;
-            bentoLink: string;
-            backgroundImageLink: string;
-        };
+    type FormattedWallpaper = {
+      wallpaperId: string;
+      githubUsername: string;
+      bentoLink: string;
+      backgroundImageLink: string;
+    };
 
-        const formattedWallpapers: {
-            extension: FormattedWallpaper[];
-            mobile: FormattedWallpaper[];
-            desktop: FormattedWallpaper[];
-        } = {
-            extension: [],
-            mobile: [],
-            desktop: [],
-        };
+    const formattedWallpapers: {
+      extension: FormattedWallpaper[];
+      mobile: FormattedWallpaper[];
+      desktop: FormattedWallpaper[];
+    } = {
+      extension: [],
+      mobile: [],
+      desktop: [],
+    };
 
-        wallpapers.forEach(wallpaper => {
-            const entry: FormattedWallpaper = {
-                wallpaperId: wallpaper.id,
-                githubUsername: wallpaper.githubUsername,
-                bentoLink: wallpaper.bentoLink,
-                backgroundImageLink: wallpaper.backgroundImageLink,
-            };
+    wallpapers.forEach((wallpaper) => {
+      const entry: FormattedWallpaper = {
+        wallpaperId: wallpaper.id,
+        githubUsername: wallpaper.githubUsername,
+        bentoLink: wallpaper.bentoLink,
+        backgroundImageLink: wallpaper.backgroundImageLink,
+      };
 
-            if (wallpaper.platform === 'EXTENSION') {
-                formattedWallpapers.extension.push(entry);
-            } else if (wallpaper.platform === 'MOBILE') {
-                formattedWallpapers.mobile.push(entry);
-            } else if (wallpaper.platform === 'DESKTOP') {
-                formattedWallpapers.desktop.push(entry);
-            }
-        });
+      if (wallpaper.platform === "EXTENSION") {
+        formattedWallpapers.extension.push(entry);
+      } else if (wallpaper.platform === "MOBILE") {
+        formattedWallpapers.mobile.push(entry);
+      } else if (wallpaper.platform === "DESKTOP") {
+        formattedWallpapers.desktop.push(entry);
+      }
+    });
 
-        return NextResponse.json({ success: true, wallpapers: formattedWallpapers }, { status: 200 });
-    } catch (error) {
-        console.error("Wallpaper Fetching error:", error);
-        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
-    }
-
+    return NextResponse.json(
+      { success: true, wallpapers: formattedWallpapers },
+      {
+        status: 200,
+        headers: {
+          "x-cache-wallpapers": JSON.stringify(formattedWallpapers),
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Wallpaper Fetching error:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
--- src/components/Dashboard/WallpaperPreview.tsx
+++ src/components/Dashboard/WallpaperPreview.tsx
@@ -1,117 +1,142 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-import MiniButton from "../ui/MiniButton"
-import { LucideTrash2, Presentation } from "lucide-react"
+import MiniButton from "../ui/MiniButton";
+import { LucideTrash2, Presentation } from "lucide-react";
 import BrowserMockup from "../magicui/Mockups/BrowserMockup";
 // import SmallIPhoneMockup from "../magicui/Mockups/SmallIphoneMockup";
 // import DesktopMockup from "../magicui/Mockups/DesktopMockup";
 import PreviewModal from "../ui/Modals/PreviewModal";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import axios from "axios";
 import { toast } from "sonner";
 
 interface IWallpaperPreview {
-    wallpaperId: string;
-    platformOf: string;
-    githubUsername: string;
-    userId: string | undefined;
-    bentoLink: string;
-    backgroundImageLink: string;
+  wallpaperId: string;
+  platformOf: string;
+  githubUsername: string;
+  userId: string | undefined;
+  bentoLink: string;
+  backgroundImageLink: string;
 }
 
 const WallpaperPreview = (props: IWallpaperPreview) => {
-    const router = useRouter()
-    const [isModalOpen, setIsModalOpen] = useState(false)
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const previewWallpaper = () => {
-        setIsModalOpen(true)
-    }
+  const previewWallpaper = () => {
+    setIsModalOpen(true);
+  };
 
-    const platformOf = props.platformOf
-    const wallpaperId = props.wallpaperId
-    const githubUsername = props.githubUsername
+  const platformOf = props.platformOf;
+  const wallpaperId = props.wallpaperId;
+  const githubUsername = props.githubUsername;
 
-    const deleteWallpaper = async () => {
-        try {
-            await axios.post("/api/v1/wallpaper/deleteWallpaper",
-                { wallpaperId, platformOf },
-                {
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                })
-
-            toast.success("Wallpaper deleted successfully")
-            window.location.reload()
-        } catch (error) {
-            toast.error("Failed to delete wallpaper")
-            console.error(error)
+  const deleteWallpaper = async () => {
+    try {
+      await axios.post(
+        "/api/v1/wallpaper/deleteWallpaper",
+        { wallpaperId, platformOf },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    }
+      );
 
-    const modalContent = {
-        extension: (
-            <div className="w-72 rounded-xl mr-6">
-                <BrowserMockup bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} onClick={previewWallpaper} />
-            </div>
-        ),
-        mobile: (
-            <div className="rounded-xl mr-6">
-                {/* <SmallIPhoneMockup bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} onClick={previewWallpaper} /> */}
-            </div>
-        ),
-        desktop: (
-            <div className="w-72 rounded-xl mr-6">
-                {/* <DesktopMockup bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} onClick={previewWallpaper} /> */}
-            </div>
-        )
+      localStorage.removeItem("cachedWallpapers");
+      toast.success("Wallpaper deleted successfully");
+      window.location.reload();
+    } catch (error) {
+      toast.error("Failed to delete wallpaper");
+      console.error(error);
+    }
+  };
+
+  const modalContent =
+    {
+      extension: (
+        <div className="w-72 rounded-xl mr-6">
+          <BrowserMockup
+            bentoLink={props.bentoLink}
+            backgroundImageLink={props.backgroundImageLink}
+            onClick={previewWallpaper}
+          />
+        </div>
+      ),
+      mobile: (
+        <div className="rounded-xl mr-6">
+          {/* <SmallIPhoneMockup bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} onClick={previewWallpaper} /> */}
+        </div>
+      ),
+      desktop: (
+        <div className="w-72 rounded-xl mr-6">
+          {/* <DesktopMockup bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} onClick={previewWallpaper} /> */}
+        </div>
+      ),
     }[props.platformOf] || null; // Default to null if no match
 
+  return (
+    <div className="relative">
+      {modalContent}
+      <div className="absolute top-0 right-6 m-1 z-30 flex">
+        <MiniButton
+          text="Get Preview Link"
+          variant="default"
+          className="mx-1"
+          onClickFunction={() => {
+            router.push(`/preview/${githubUsername}/${wallpaperId}`);
+          }}
+        >
+          <Presentation className="h-4 w-4" />
+        </MiniButton>
 
-    return (
-        <div className="relative">
-            {modalContent}
-            <div className="absolute top-0 right-6 m-1 z-30 flex">
-                <MiniButton text="Get Preview Link" variant="default" className="mx-1" onClickFunction={() => { router.push(`/preview/${githubUsername}/${wallpaperId}`) }}><Presentation className="h-4 w-4" /></MiniButton>
-
-                <AlertDialog>
-                    <AlertDialogTrigger asChild>
-                        <div>
-                            <MiniButton text="Delete" variant="destructive">
-                                <LucideTrash2 className="h-4 w-4" />
-                            </MiniButton>
-                        </div>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                        <AlertDialogHeader>
-                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                            <AlertDialogDescription>
-                                This action cannot be undone. This will permanently delete this
-                                wallpaper and remove your wallpaper from our servers.
-                            </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction onClick={() => deleteWallpaper()}>Continue</AlertDialogAction>
-                        </AlertDialogFooter>
-                    </AlertDialogContent>
-                </AlertDialog>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <div>
+              <MiniButton text="Delete" variant="destructive">
+                <LucideTrash2 className="h-4 w-4" />
+              </MiniButton>
             </div>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete this
+                wallpaper and remove your wallpaper from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={() => deleteWallpaper()}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
 
-            {isModalOpen && <PreviewModal bentoLink={props.bentoLink} backgroundImageLink={props.backgroundImageLink} closeModal={() => setIsModalOpen(false)} platformOf={props.platformOf} />}
-        </div>
-    )
-}
+      {isModalOpen && (
+        <PreviewModal
+          bentoLink={props.bentoLink}
+          backgroundImageLink={props.backgroundImageLink}
+          closeModal={() => setIsModalOpen(false)}
+          platformOf={props.platformOf}
+        />
+      )}
+    </div>
+  );
+};
 
-export default WallpaperPreview
+export default WallpaperPreview;
