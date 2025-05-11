dfa7f2d1cc20e6ce843479da3b4e74f9de9e45f8
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-05-11 07:43:23 +0530

    watermark in bento shadow fix

--- src/components/BentoComponents/BentoLayout/Bento.tsx
+++ src/components/BentoComponents/BentoLayout/Bento.tsx
@@ -42,68 +42,69 @@ const Bento = ({ bentoComponentRef }: { bentoComponentRef: React.RefObject<HTMLD
                 borderRadius: backgroundImageEntry?.imgUrl ? "0px" : "20px"
             }}
         >
-            <div
-                ref={bentoComponentRef}
-                className="h-164 w-264 p-2 rounded-3xl relative"
-                style={{
-                    background: `linear-gradient(to bottom right, ${currentPalette.main4}, ${currentPalette.main2}, ${currentPalette.main4})`,
-                    boxShadow: backgroundImageEntry?.imgUrl ? '' : `0px 10px 20px -3px ${currentPalette.main3}`,
-                    color: `${currentPalette.textColor}`,
-                }}
-            >
-                <div className="h-full w-full rounded-2xl flex flex-col p-2"
-                    style={{ background: `${currentPalette.bgColor}` }}>
-                    {/* upperhalf section */}
-                    <div className='flex w-full h-7/10'>
-                        <div className='h-full w-8/10'>
-                            <div className='flex h-1/2 w-full'>
-                                <div className='flex flex-col w-2/12'>
-                                    <GithubAvatar />
-                                    <ImageContainer position='Top Left' />
+            <div ref={bentoComponentRef} className='p-7'>
+                <div
+                    className="h-164 w-264 p-2 rounded-3xl relative"
+                    style={{
+                        background: `linear-gradient(to bottom right, ${currentPalette.main4}, ${currentPalette.main2}, ${currentPalette.main4})`,
+                        boxShadow: backgroundImageEntry?.imgUrl ? '' : `0px 10px 20px -3px ${currentPalette.main3}`,
+                        color: `${currentPalette.textColor}`,
+                    }}
+                >
+                    <div className="h-full w-full rounded-2xl flex flex-col p-2"
+                        style={{ background: `${currentPalette.bgColor}` }}>
+                        {/* upperhalf section */}
+                        <div className='flex w-full h-7/10'>
+                            <div className='h-full w-8/10'>
+                                <div className='flex h-1/2 w-full'>
+                                    <div className='flex flex-col w-2/12'>
+                                        <GithubAvatar />
+                                        <ImageContainer position='Top Left' />
+                                    </div>
+                                    <div className='ml-2 w-4/12'>
+                                        <PersonalInfo />
+                                    </div>
+                                    <div className='grid grid-cols-2 ml-2 gap-2 w-4/12'>
+                                        <Location />
+                                        <TotalRepoCount />
+                                        <FollowerCount />
+                                        <ImageContainer position='Top Right' />
+                                    </div>
+                                    <div className='w-2/12'>
+                                        <CurrentStreak />
+                                    </div>
                                 </div>
-                                <div className='ml-2 w-4/12'>
-                                    <PersonalInfo />
-                                </div>
-                                <div className='grid grid-cols-2 ml-2 gap-2 w-4/12'>
-                                    <Location />
-                                    <TotalRepoCount />
-                                    <FollowerCount />
-                                    <ImageContainer position='Top Right' />
-                                </div>
-                                <div className='w-2/12'>
-                                    <CurrentStreak />
+                                <div className='flex w-full h-1/2 py-2'>
+                                    <div className='w-2/10 h-full'>
+                                        <LongestStreak />
+                                    </div>
+                                    <div className='grid grid-cols-2 gap-2 mr-2 w-4/10'>
+                                        <Commits />
+                                        <TotalPRs />
+                                        <TotalStars />
+                                        <TotalCommits />
+                                    </div>
+                                    <div className='w-4/10'>
+                                        <TotalContributionAndOrgs />
+                                    </div>
                                 </div>
                             </div>
-                            <div className='flex w-full h-1/2 py-2'>
-                                <div className='w-2/10 h-full'>
-                                    <LongestStreak />
-                                </div>
-                                <div className='grid grid-cols-2 gap-2 mr-2 w-4/10'>
-                                    <Commits />
-                                    <TotalPRs />
-                                    <TotalStars />
-                                    <TotalCommits />
-                                </div>
-                                <div className='w-4/10'>
-                                    <TotalContributionAndOrgs />
-                                </div>
+                            <div className='w-2/10 ml-2 mb-2'>
+                                <ImageContainer position='Right Side' />
                             </div>
                         </div>
-                        <div className='w-2/10 ml-2 mb-2'>
-                            <ImageContainer position='Right Side' />
+
+                        {/* heatmap section */}
+                        <div className='flex w-full h-3/10'>
+                            <div className='mr-2 w-full'>
+                                <ImageContainer position='Bottom Left' />
+                            </div>
+                            <Heatmap />
                         </div>
                     </div>
 
-                    {/* heatmap section */}
-                    <div className='flex w-full h-3/10'>
-                        <div className='mr-2 w-full'>
-                            <ImageContainer position='Bottom Left' />
-                        </div>
-                        <Heatmap />
-                    </div>
+                    <div className='absolute -bottom-1 right-[38%]'><Watermark /></div>
                 </div>
-
-                <div className='absolute -bottom-1 right-[38%]'><Watermark /></div>
             </div>
         </div>
     )
