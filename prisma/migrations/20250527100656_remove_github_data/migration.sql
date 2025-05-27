12a156f9305290e74bd88471454fb730a78189dc
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-05-27 16:37:29 +0530

    ♻️ refactor: githubData was not needed so removed it

---
+++ prisma/migrations/20250527100656_remove_github_data/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `githubData` on the `UserWallpaper` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserWallpaper" DROP COLUMN "githubData";
