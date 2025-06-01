de5e92af65907a0af902aa0fc6fc4cb35632adb9
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-06-01 22:25:29 +0530

    configs for cron job in vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Add your existing config options here
-
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.externals.push({
-        'puppeteer-core': 'commonjs puppeteer-core',
-      });
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core'],
+  },
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
     }
     return config;
   },
